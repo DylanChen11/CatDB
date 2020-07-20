b24573a11c0692e91f4cb1a64267a60fd9b5336f
Dylan Chen <dylanchen11@yahoo.co.uk>
2020-07-20 16:17:57 +0800

    Changed the serializers to use HyperlinkedModelSerializers instead of ModelSerializers

--- exercise/catdb/serializers.py
+++ exercise/catdb/serializers.py
@@ -2,22 +2,25 @@ from rest_framework import serializers
 from catdb.models import Home, Cat, Breed, Human
 
 
-class HomeSerializer(serializers.ModelSerializer):
+class HomeSerializer(serializers.HyperlinkedModelSerializer):
+    humans=serializers.HyperlinkedRelatedField(many=True, view_name='human-detail', read_only=True)
     class Meta:
         model=Home
-        fields=['id', 'name', 'address', 'Type']
+        fields=['id', 'name', 'address', 'Type', 'humans']
 
-class HumanSerializer(serializers.ModelSerializer):
+class HumanSerializer(serializers.HyperlinkedModelSerializer):
+    cats=serializers.HyperlinkedRelatedField(many=True, view_name='cat-detail', read_only=True)
     class Meta:
         model=Human
-        fields=['id', 'name', 'gender', 'dob', 'description', 'home']
+        fields=['id', 'name', 'gender', 'dob', 'description', 'home', 'cats']
 
-class CatSerializer(serializers.ModelSerializer):
+class CatSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=Cat
         fields=['id', 'name', 'gender', 'dob', 'description', 'breed', 'owner']
 
-class BreedSerializer(serializers.ModelSerializer):
+class BreedSerializer(serializers.HyperlinkedModelSerializer):
+    cats=serializers.HyperlinkedRelatedField(many=True, view_name='cat-detail', read_only=True)
     class Meta:
         model=Breed
-        fields=['id', 'name', 'origin', 'description', 'homes']
+        fields=['id', 'name', 'origin', 'description', 'homes', 'cats']
