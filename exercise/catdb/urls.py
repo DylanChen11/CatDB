9307940be9db294f48158b9f934886a56485116e
Dylan Chen <dylanchen11@yahoo.co.uk>
2020-07-20 16:16:56 +0800

    added a path for api root, and a name field for all other paths to allow the serializer to refer to it by name

--- exercise/catdb/urls.py
+++ exercise/catdb/urls.py
@@ -3,14 +3,15 @@ from catdb import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('home/', views.HomeList.as_view()),
-    path('home/<int:pk>/', views.HomeDetail.as_view()),
-    path('human/', views.HumanList.as_view()),
-    path('human/<int:pk>/', views.HumanDetail.as_view()),
-    path('cat/', views.CatList.as_view()),
-    path('cat/<int:pk>/', views.CatDetail.as_view()),
-    path('breed/', views.BreedList.as_view()),
-    path('breed/<int:pk>/', views.BreedDetail.as_view()),
+    path('', views.api_root),
+    path('home/', views.HomeList.as_view(), name='home-list'),
+    path('home/<int:pk>/', views.HomeDetail.as_view(), name='home-detail'),
+    path('human/', views.HumanList.as_view(), name='human-list'),
+    path('human/<int:pk>/', views.HumanDetail.as_view(), name='human-detail'),
+    path('cat/', views.CatList.as_view(), name='cat-list'),
+    path('cat/<int:pk>/', views.CatDetail.as_view(), name='cat-detail'),
+    path('breed/', views.BreedList.as_view(), name='breed-list'),
+    path('breed/<int:pk>/', views.BreedDetail.as_view(), name='breed-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
