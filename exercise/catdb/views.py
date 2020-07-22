872709325703d9ac3a11289fa935364ea0aa55fd
Dylan Chen <dylanchen11@yahoo.co.uk>
2020-07-22 16:24:22 +0800

    changed from using views to viewsets which reduces repeated code and number of classes

--- exercise/catdb/views.py
+++ exercise/catdb/views.py
@@ -5,225 +5,65 @@ from catdb.serializers import HomeSerializer, HumanSerializer, CatSerializer, Br
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+
+from rest_framework import viewsets
 # Create your views here.
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'home': reverse('home-list', request=request, format=format),
-        'human': reverse('human-list', request=request, format=format),
-        'cat':reverse('cat-list', request=request, format=format),
-        'breed': reverse('breed-list', request=request, format=format),
-    })
 
-class HomeList(generics.ListCreateAPIView):
+class HomeViewSet(viewsets.ModelViewSet):
+    # This viewset automatically provides `list`, `create`, `retrieve`,
+    # `update` and `destroy` actions.
     queryset = Home.objects.all()
     serializer_class = HomeSerializer
 
 
-class HomeDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Home.objects.all()
-    serializer_class = HomeSerializer
-
-class HumanList(generics.ListCreateAPIView):
+class HumanViewSet(viewsets.ModelViewSet):
     queryset = Human.objects.all()
     serializer_class = HumanSerializer
 
 
-class HumanDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Human.objects.all()
-    serializer_class = HumanSerializer
-
-class CatList(generics.ListCreateAPIView):
+class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
 
 
-class CatDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Cat.objects.all()
-    serializer_class = CatSerializer
-
-class BreedList(generics.ListCreateAPIView):
+class BreedViewSet(viewsets.ModelViewSet):
     queryset = Breed.objects.all()
     serializer_class = BreedSerializer
 
 
-class BreedDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Breed.objects.all()
-    serializer_class = BreedSerializer
-# @csrf_exempt
-# def homes_list(request):
-#     """
-#     List all houses, or create a new house.
-#     """
-#     if request.method == 'GET':
-#         homes = Home.objects.all()
-#         serializer = HomeSerializer(homes, many=True)
-#         return JsonResponse(serializer.data, safe=False)
-
-#     elif request.method == 'POST':
-#         data = JSONParser().parse(request)
-#         serializer = HomeSerializer(data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data, status=201)
-#         return JsonResponse(serializer.errors, status=400)
-
-# @csrf_exempt
-# def home_detail(request, pk):
-#     """
-#     Retrieve, update or delete a house instance.
-#     """
-#     try:
-#         home = Home.objects.get(pk=pk)
-#     except home.DoesNotExist:
-#         return HttpResponse(status=404)
-
-#     if request.method == 'GET':
-#         serializer = HomeSerializer(home)
-#         return JsonResponse(serializer.data)
-
-#     elif request.method == 'PUT':
-#         data = JSONParser().parse(request)
-#         serializer = HomeSerializer(home, data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data)
-#         return JsonResponse(serializer.errors, status=400)
-
-#     elif request.method == 'DELETE':
-#         home.delete()
-#         return HttpResponse(status=204)
+# class HomeList(generics.ListCreateAPIView):
+#     queryset = Home.objects.all()
+#     serializer_class = HomeSerializer
 
 
-# @csrf_exempt
-# def human_list(request):
-#     """
-#     List all houses, or create a new human.
-#     """
-#     if request.method == 'GET':
-#         humans = Human.objects.all()
-#         serializer = HumanSerializer(humans, many=True)
-#         return JsonResponse(serializer.data, safe=False)
+# class HomeDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Home.objects.all()
+#     serializer_class = HomeSerializer
 
-#     elif request.method == 'POST':
-#         data = JSONParser().parse(request)
-#         serializer = HumanSerializer(data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data, status=201)
-#         return JsonResponse(serializer.errors, status=400)
+# class HumanList(generics.ListCreateAPIView):
+#     queryset = Human.objects.all()
+#     serializer_class = HumanSerializer
 
-# @csrf_exempt
-# def human_detail(request, pk):
-#     """
-#     Retrieve, update or delete a human instance.
-#     """
-#     try:
-#         human = Human.objects.get(pk=pk)
-#     except human.DoesNotExist:
-#         return HttpResponse(status=404)
 
-#     if request.method == 'GET':
-#         serializer = HumanSerializer(human)
-#         return JsonResponse(serializer.data)
+# class HumanDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Human.objects.all()
+#     serializer_class = HumanSerializer
 
-#     elif request.method == 'PUT':
-#         data = JSONParser().parse(request)
-#         serializer = HumanSerializer(human, data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data)
-#         return JsonResponse(serializer.errors, status=400)
+# class CatList(generics.ListCreateAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
 
-#     elif request.method == 'DELETE':
-#         human.delete()
-#         return HttpResponse(status=204)
 
-# @csrf_exempt
-# def cat_list(request):
-#     """
-#     List all houses, or create a new cat.
-#     """
-#     if request.method == 'GET':
-#         cats = Cat.objects.all()
-#         serializer = CatSerializer(cats, many=True)
-#         return JsonResponse(serializer.data, safe=False)
+# class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
 
-#     elif request.method == 'POST':
-#         data = JSONParser().parse(request)
-#         serializer = CatSerializer(data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data, status=201)
-#         return JsonResponse(serializer.errors, status=400)
+# class BreedList(generics.ListCreateAPIView):
+#     queryset = Breed.objects.all()
+#     serializer_class = BreedSerializer
 
-# @csrf_exempt
-# def cat_detail(request, pk):
-#     """
-#     Retrieve, update or delete a cat instance.
-#     """
-#     try:
-#         cat = Cat.objects.get(pk=pk)
-#     except cat.DoesNotExist:
-#         return HttpResponse(status=404)
 
-#     if request.method == 'GET':
-#         serializer = CatSerializer(cat)
-#         return JsonResponse(serializer.data)
-
-#     elif request.method == 'PUT':
-#         data = JSONParser().parse(request)
-#         serializer = CatSerializer(cat, data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data)
-#         return JsonResponse(serializer.errors, status=400)
-
-#     elif request.method == 'DELETE':
-#         cat.delete()
-#         return HttpResponse(status=204)
-
-# @csrf_exempt
-# def breed_list(request):
-#     """
-#     List all houses, or create a new breed.
-#     """
-#     if request.method == 'GET':
-#         breeds = Breed.objects.all()
-#         serializer = BreedSerializer(breeds, many=True)
-#         return JsonResponse(serializer.data, safe=False)
-
-#     elif request.method == 'POST':
-#         data = JSONParser().parse(request)
-#         serializer = BreedSerializer(data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data, status=201)
-#         return JsonResponse(serializer.errors, status=400)
-
-# @csrf_exempt
-# def breed_detail(request, pk):
-#     """
-#     Retrieve, update or delete a breed instance.
-#     """
-#     try:
-#         breed = Breed.objects.get(pk=pk)
-#     except breed.DoesNotExist:
-#         return HttpResponse(status=404)
-
-#     if request.method == 'GET':
-#         serializer = BreedSerializer(breed)
-#         return JsonResponse(serializer.data)
-
-#     elif request.method == 'PUT':
-#         data = JSONParser().parse(request)
-#         serializer = BreedSerializer(breed, data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data)
-#         return JsonResponse(serializer.errors, status=400)
-
-#     elif request.method == 'DELETE':
-#         breed.delete()
-#         return HttpResponse(status=204) 
+# class BreedDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Breed.objects.all()
+#     serializer_class = BreedSerializer
